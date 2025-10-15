4422f29b01cff4ccd4df5380611ec45fe84a9e9c
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-10-15 02:27:08 -0700

    Yeehaw.

--- src/com/badlogic/gdx/graphics/g2d/freetype/gwt/emu/com/badlogic/gdx/graphics/g2d/freetype/FreeTypeFontGenerator.java
+++ src/com/badlogic/gdx/graphics/g2d/freetype/gwt/emu/com/badlogic/gdx/graphics/g2d/freetype/FreeTypeFontGenerator.java
@@ -594,8 +594,8 @@ public class FreeTypeFontGenerator implements Disposable {
 
 		PixmapPacker.PixmapPackerRectangle rect = packer.pack(mainPixmap);
 		glyph.page = packer.getPages().size - 1; // Glyph is always packed into the last page for now.
-		glyph.srcX = (int)rect.x;
-		glyph.srcY = (int)rect.y;
+		glyph.srcX = rect.getX();
+		glyph.srcY = rect.getY();
 
 		// If a page was added, create a new texture region for the incrementally added glyph.
 		if (parameter.incremental && data.regions != null && data.regions.size <= glyph.page)
